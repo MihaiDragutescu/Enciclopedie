fc59c417120af8c533ec38c2cb0a3d39bfc4cf6d
Mihai <mihaidragutescu36@gmail.com>
2021-01-04 19:44:50 +0200

    Updated commit.

--- Enciclopedie/Models/ManageViewModels.cs
+++ Enciclopedie/Models/ManageViewModels.cs
@@ -43,18 +43,18 @@ namespace Enciclopedie.Models
     {
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Parola curenta")]
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "{0} trebuie sa aiba cel putin {2} caractere.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Noua parola")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Confirmati noua parola")]
+        [Compare("NewPassword", ErrorMessage = "Parolele nu se potrivesc.")]
         public string ConfirmPassword { get; set; }
     }
 
